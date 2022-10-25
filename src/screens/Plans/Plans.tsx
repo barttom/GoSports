22027da66ef96be06c52276ffdb557467594b5bf
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2022-10-25 21:12:23 +0200

    added basic stack navigation

---
+++ src/screens/Plans/Plans.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card, Text} from 'react-native-paper';
+// import {Card, Text} from 'react-native-paper';
+
+// export type PlansProps = {};
+
+export const Plans = (/*{}: PlansProps*/) => {
+  return (
+    <Card>
+      <Text>Plans!</Text>
+    </Card>
+  );
+};
