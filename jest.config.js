de05eb99da44e9f91f234f0770ad2363a32817e6
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2022-10-26 10:23:10 +0200

    fix tests warning

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|react-native-paper)',
   ],
+  setupFiles: ['./jest.mock.js'],
 };
