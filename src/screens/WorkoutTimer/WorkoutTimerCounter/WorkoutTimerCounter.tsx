b9e747757d8fd58769fd57d5318ceed3f6e0a755
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2022-11-14 08:02:51 +0100

    display items list of chosen workout

---
+++ src/screens/WorkoutTimer/WorkoutTimerCounter/WorkoutTimerCounter.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {List} from 'react-native-paper';
+import {FlatList} from 'react-native';
+import {WorkoutItemAttrs} from '../../../realm/objects/WorkoutItem';
+import {useMakeStyles} from '../../../hooks/useMakeStyles';
+
+export type WorkoutTimerCounterProps = {items: WorkoutItemAttrs[]};
+
+export const WorkoutTimerCounter = ({items}: WorkoutTimerCounterProps) => {
+  const {styles} = useMakeStyles(() => ({
+    itemShadowed: {
+      opacity: 0.5,
+    },
+  }));
+
+  return (
+    <FlatList
+      data={items}
+      renderItem={({item: {exercise, sets}}) => {
+        const data = sets[0];
+
+        return (
+          <List.Item
+            style={styles.itemShadowed}
+            title={exercise.title}
+            description={`${data.series} series | ${data.reps} reps | ${data.weightKg}kg`}
+          />
+        );
+      }}
+    />
+  );
+};
