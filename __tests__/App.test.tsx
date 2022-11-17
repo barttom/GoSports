a6ff48863d28a907d502a308e2ab0dde2213ef55
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2022-11-17 14:36:19 +0100

    fixed tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -8,6 +8,11 @@ import '@testing-library/jest-native/extend-expect';
 import {render, screen, waitFor} from '@testing-library/react-native';
 import {App} from '../src/App';
 
+jest.mock('react-native-background-timer', () => {});
+jest.mock('@notifee/react-native', () => ({
+  addListener: () => jest.fn(),
+}));
+
 it('renders correctly', async () => {
   render(<App />);
   await waitFor(() => {
